3ac4c50098800262eb7808f6c7e1f44d28f1097b
Thiago Santos <thiago_593@hotmail.com>
2022-11-04 11:35:45 -0300

    correção

---
+++ Exercicios/teste/Casos-teste/cadastrousuario.java
@@ -0,0 +1,17 @@
+public class cadastro {
+    @io.cucumber.java.pt.Quando("^eu quero cadastrar o seguinte usuário$")
+    public void euQueroCadastrarOSeguinteUsuário() {
+    }
+
+    @io.cucumber.java.pt.Então("^eu vejo: Cadastro realizado com sucesso$")
+    public void euVejoCadastroRealizadoComSucesso() {
+    }
+
+    @io.cucumber.java.pt.Quando("^um usuário já possui cadastro$")
+    public void umUsuárioJáPossuiCadastro() {
+    }
+
+    @io.cucumber.java.pt.Então("^eu vejo já existe um usuário cadastrado com essa matrícula$")
+    public void euVejoJáExisteUmUsuárioCadastradoComEssaMatrícula() {
+    }
+}
